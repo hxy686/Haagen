309582fa4fa71cfa00366eabc68ba6d3cf67168d
hxy686 <2029786242@qq.com>
2020-09-11 17:10:59 +0800

    v1.0

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+let gulp = require('gulp');
+let concat = require('gulp-concat');
+let htmlmin = require('gulp-htmlmin');
+let imagemin = require('gulp-imagemin');
+let rename = require('gulp-rename');
+let uglify = require('gulp-uglify');
+let cssnano = require('gulp-cssnano');
+let babel = require('gulp-babel');
+let sass = require('gulp-sass');
+
+//发布任务
+function fnJs(){
+    return gulp.src('./src/js/*.js')
+        .pipe(babel({
+            presets : ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(rename({suffix : '.min'}))
+        .pipe(gulp.dest('./dist/js'));
+}
+
+function fnImg(){
+    return gulp.src('./src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'))
+}
+function fnWatch(){
+    gulp.watch('./src/js/*.js',fnJs)
+    gulp.watch('./src/Sass/*.scss',fnSass)
+    gulp.watch('./src/index.html',fnCopy)
+}
+
+function fnCopy(){
+    return gulp.src('./src/index.html')
+        .pipe(gulp.dest('./dist/'))
+}
+
+function fnSass(){
+    return gulp.src('./src/Sass/*.scss')
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(rename({suffix : '.min'}))
+        .pipe(gulp.dest('./dist/css'))
+}
+//导出任务
+exports.js = fnJs;
+exports.img = fnImg;
+exports.default = fnWatch;
+exports.copy = fnCopy;
+exports.sass = fnSass;
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+var mySwiper = new Swiper('.swiper-container',{
+	pagination : '.swiper-pagination',
+	paginationElement : 'li',
+	autoplay : 500,
+loop : true,
+})
