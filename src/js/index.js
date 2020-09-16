c67be35829719059c4c8f5e7dab96864a01ce0ee
hxy686 <2029786242@qq.com>
2020-09-16 16:33:05 +0800

    v1.08
    增加了列表页

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,41 @@ $(function(){
 	loop : true,
 	})
 	
+	// 初始化 第一个 状态
+	jQuery('.dh ul li.curr').animate({width:'246px'},'slow');
+	jQuery('.dh ul li.curr .layer').animate({height:'60px'},'slow');
+	jQuery('.dh ul li.curr .layer .p1').animate({left:'-200%'},'slow');
+	jQuery('.dh ul li.curr .layer .p2').animate({right:'145px'},'slow');
+
+	jQuery('.sfq ul li').hover(function(){
+	  // 获取索引
+	   var _index = $(this).index();
+
+	   $(this).addClass('curr')
+	   .stop()
+	   .animate({
+		  width:'246px'
+	   },'slow')
+	   .siblings()
+	   .stop()
+	   .animate({
+		  width:'200px'
+	   },'slow')
+	   .removeClass('curr');
+
+	   $(this).find('.layer').stop().animate({height:'60px'},'slow').parent().siblings().find('.layer').stop().animate({
+		   height: '100%'
+	   },'slow');
+
+	   $(this).find('.layer .p1').stop().animate({left:'-200%'},'slow').siblings('.p2').stop().animate({
+		   right: '145px'
+	   },'slow').parent().parent().siblings().find('.layer .p1').stop().animate({left:'10px'},'slow').siblings('.p2').stop().animate({
+		   right: '-200%'
+	   },'slow')
+
+	},function(){
+	  //移出
+
+	})
 })
 
