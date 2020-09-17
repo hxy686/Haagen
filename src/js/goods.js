8f610a9ff7dfdd936bd1007b37b6a120638f3fda
hxy686 <2029786242@qq.com>
2020-09-17 17:17:08 +0800

    1.09
    只剩购物车页面

---
+++ src/js/goods.js
@@ -0,0 +1,14 @@
+$(function(){
+    $(document).ready(function () {
+        $("#green_monster").mlens(
+        {
+            imgSrc: $("#green_monster").attr("data-big"),	// path of the hi-res version of the image
+            lensShape: "square",				// shape of the lens (circle or square)
+            lensSize: 200,					// size of the lens (in px)
+            borderSize: 1,					// size of the lens border (in px)
+            borderColor: "#fff",				// color of the lens border (#hex)
+            borderRadius: 0	,				// border radius (optional, only if the shape is square)
+        });
+    });
+    $('.spinnerExample').spinner({});
+})
